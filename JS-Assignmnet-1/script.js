2cc6d020bad0ce9f729bfe30e54cc3f6f8f8f4ec
HusnainHaider21 <husnainhaider0304@gmail.com>
2024-05-09 14:57:35 +0500

    7 program completed

--- JS-Assignmnet-1/script.js
+++ JS-Assignmnet-1/script.js
@@ -32,3 +32,35 @@ if (num > 0) {
 } else {
     console.log(num,"is zero");
 }
+
+/* 4. Write a js program to check whether a number is divisible by 5 and 11 or not.*/
+let number = 55;
+if (number % 5==0 && number % 11 ==0) {
+    console.log(number,"is divisble by 5 and 11");
+} else {
+    console.log(number,"is not divisible by 5 and 11");
+}
+
+/* 5. Write a js program to check whether a number is even or odd.*/
+if (num1 % 2==0) {
+    console.log(num1,"is even");
+} else {
+    console.log(num1,"is odd");
+}
+
+/* 6. Write a js program to check whether a year is leap year or not.*/
+let year = 2024;
+if (year % 4==0 && year % 100!==0 ||year % 400==0) {
+    console.log(year,"is leap year");
+} else {
+    console.log(year,"is not leap year");
+}
+
+/* 7. Write a js program to input any alphabet and check whether it is vowel or
+consonant.*/
+let userAlphabet = prompt("Enter any Alphabet","E").toUpperCase();
+if (userAlphabet === "A" ||userAlphabet === "E" ||userAlphabet === "I" ||userAlphabet ==="O" ||userAlphabet ==="U") {
+    console.log(userAlphabet,"is a vowel");
+} else {
+    console.log(userAlphabet,"is not a vowel");
+}
